a852d05ac6efce3bded8879a93a1c3ba6fcd2f3a
Anton <anton@adc.sh>
2018-05-24 17:15:51 +0100

    build

--- build/index.js
+++ build/index.js
@@ -3,8 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = context;
-exports.SnapshotContext = void 0;
+exports.default = void 0;
 
 var _path = require("path");
 
@@ -25,111 +24,119 @@ var _erotic = _interopRequireDefault(require("erotic"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 const isJSON = p => /\.json$/.test(p);
+/**
+ * SnapshotContext allows to match the test result against a snapshot.
+ */
 
-async function context() {
-  let snapshotsDir = '';
-  Object.assign(this, {
-    setDir(dir) {
-      snapshotsDir = dir;
-    },
 
-    save: async (path, snapshot) => {
-      const p = (0, _path.resolve)(snapshotsDir, path);
-      await (0, _wrote.ensurePath)(p);
+class Context {
+  constructor() {
+    this.snapshotsDir = '';
+  }
+  /**
+   * Set the directory to save snapshots in.
+   * @param {string} dir The directory.
+   */
 
-      if (isJSON(p)) {
-        await (0, _wrote.writeJSON)(p, snapshot, {
-          space: 2
-        });
+
+  setDir(dir) {
+    this.snapshotsDir = dir;
+  }
+
+  async save(path, snapshot) {
+    const p = (0, _path.resolve)(this.snapshotsDir, path);
+    await (0, _wrote.ensurePath)(p);
+
+    if (isJSON(p)) {
+      await (0, _wrote.writeJSON)(p, snapshot, {
+        space: 2
+      });
+    } else {
+      const ws = await (0, _wrote.createWritable)(p);
+      await (0, _wrote.write)(ws, snapshot);
+    }
+  }
+
+  async prompt(snapshot) {
+    if (typeof snapshot == 'string') {
+      console.log(snapshot); // eslint-disable-line no-console
+    } else {
+      console.log((0, _util.inspect)(snapshot, {
+        colors: true
+      })); // eslint-disable-line
+    }
+
+    const {
+      promise
+    } = (0, _reloquent.default)('save snapshot? ');
+    const answer = await promise;
+    return answer == 'y';
+  }
+
+  async promptAndSave(path, actual, err = new Error('could not test missing snapshot')) {
+    if (!actual) throw new Error('give snapshot to save');
+    const res = await this.prompt(actual);
+
+    if (res) {
+      await this.save(path, actual);
+    } else {
+      throw err;
+    }
+  }
+
+  async read(path) {
+    const p = (0, _path.resolve)(this.snapshotsDir, path);
+
+    if (isJSON(p)) {
+      const snapshot = await (0, _wrote.readJSON)(p);
+      return snapshot;
+    } else {
+      const snapshot = await (0, _wrote.read)(p);
+      return snapshot.trim();
+    }
+  }
+  /**
+   * Test the snapshot by reading the file and matching it against the given actual value. If filename ends with .json, the data will be serialised as a JSON, and then parsed back and deep-equal will be performed. Otherwise, string comparison is made with red/green highlighting. If no file exists, a prompt will be shown to save a snapshot. Answer with **b** to accept the snapshot and pass the test. There's no update possibility which means files must be deleted by hand and new snapshots taken.
+   * @param {string} path Path to the file
+   * @param {string} actual Expected result
+   */
+
+
+  async test(path, actual) {
+    if (!actual) throw new Error('pass the actual value');
+    const cb = (0, _erotic.default)(true);
+    const json = isJSON(path);
+    let expected;
+
+    try {
+      expected = await this.read(path);
+
+      if (json) {
+        (0, _assertDiff.deepEqual)(actual, expected);
       } else {
-        const ws = await (0, _wrote.createWritable)(p);
-        await (0, _wrote.write)(ws, snapshot);
+        (0, _assert.equal)(actual, expected);
       }
-    },
-    prompt: async snapshot => {
-      if (typeof snapshot == 'string') {
-        console.log(snapshot); // eslint-disable-line no-console
-      } else {
-        console.log((0, _util.inspect)(snapshot, {
-          colors: true
-        })); // eslint-disable-line
+    } catch (err) {
+      if (err.code == 'ENOENT') {
+        await this.promptAndSave(path, actual);
+        return;
       }
 
-      const {
-        promise
-      } = (0, _reloquent.default)('save snapshot? ');
-      const answer = await promise;
-      return answer == 'y';
-    },
-    promptAndSave: async (path, actual, err = new Error('could not test missing snapshot')) => {
-      if (!actual) throw new Error('give snapshot to save');
-      const res = await this.prompt(actual);
+      if (!json) {
+        const s = (0, _erte.default)(expected, actual);
+        console.log(s); // eslint-disable-line no-console
 
-      if (res) {
-        await this.save(path, actual);
-      } else {
-        throw err;
-      }
-    },
-    read: async path => {
-      const p = (0, _path.resolve)(snapshotsDir, path);
-
-      if (isJSON(p)) {
-        const snapshot = await (0, _wrote.readJSON)(p);
-        return snapshot;
-      } else {
-        const snapshot = await (0, _wrote.read)(p);
-        return snapshot.trim();
-      }
-    },
-    test: async (path, actual) => {
-      const cb = (0, _erotic.default)(true);
-      const json = isJSON(path);
-      let expected;
-
-      try {
-        expected = await this.read(path);
-
-        if (json) {
-          (0, _assertDiff.deepEqual)(actual, expected);
-        } else {
-          (0, _assert.equal)(actual, expected);
-        }
-      } catch (err) {
-        if (err.code == 'ENOENT') {
-          await this.promptAndSave(path, actual);
-          return;
-        }
-
-        if (!json) {
-          const s = (0, _erte.default)(expected, actual);
-          console.log(s); // eslint-disable-line no-console
-
-          const e = cb('The string didn\'t match the snapshot.');
-          e.erte = s;
-          throw e;
-        }
-
-        const e = cb(err);
+        const e = cb('The string didn\'t match the snapshot.');
+        e.erte = s;
         throw e;
       }
+
+      const e = cb(err);
+      throw e;
     }
-  });
+  }
+
 }
-/**
- * @typedef {Object} SnapshotContext
- * @property {(path: string) => Promise<string>} read
- * @property {(path: string, snapshot: string) => Promise<string>} save
- * @property {(path: string) => Promise<boolean>} prompt
- * @property {(path: string, actual: string) => Promise} test Test a snapshot.
- * @property {(path: string)} setDir Set the directory to save snapshots.
- */
 
-/**
- * @type {SnapshotContext}
- */
-
-
-const SnapshotContext = {};
-exports.SnapshotContext = SnapshotContext;
+exports.default = Context;
 //# sourceMappingURL=index.js.map
