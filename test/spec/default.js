5432285fdcd99dbcc4925dc80f6d1c591c4f83c9
Anton <anton@adc.sh>
2019-04-30 18:57:39 +0300

    test deep equal w/ temp-context

--- test/spec/default.js
+++ test/spec/default.js
@@ -1,18 +1,31 @@
-import { equal } from 'zoroaster/assert'
+import { equal, throws } from 'zoroaster/assert'
+import TempContext from 'temp-context'
 import Context from '../context'
 import SnapshotContext from '../../src'
 
 /**
- * @type {Object<string, (c:Context, s:SnapshotContext)>}
+ * @type {Object<string, (c:Context, s:SnapshotContext, t:TempContext)>}
  */
 const T = {
-  context: [Context, SnapshotContext],
+  context: [Context, SnapshotContext, TempContext],
   'is a function'() {
     equal(typeof SnapshotContext, 'function')
   },
   async 'can test json snapshot'({ json }, { test }) {
     await test(json, {
       data: 'test',
+    })
+  },
+  async 'fails json snapshot'(_, { test, setDir }, { write, TEMP }) {
+    setDir(TEMP)
+    await write('snapshot.json',
+      JSON.stringify({ data: 'test-expected' }))
+    await throws({
+      fn: test,
+      args: ['snapshot.json', {
+        data: 'test-actual',
+      }],
+      message: /test-expected[\s\S]+test-actual/,
     })
   },
   async 'can test text snapshot'({ text }, { test }) {
